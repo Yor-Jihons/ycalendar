972904d25827e7ee41dab2b8480ab1faca28f9ec
Yor-Jihons <ryo.malon814@gmail.com>
2022-11-06 20:21:54 +0900

    mainDate → mainDate_first

--- ycalendar/ycalendar.js
+++ ycalendar/ycalendar.js
@@ -1,10 +1,5 @@
 let calendar = document.getElementById( "ycalendar" );
 
-/*
-let text = "";
-text = "<h2><a onclick='PrevButton_Click(2022,1)'>◀</a> 2022/2 <a onclick='NextButton_Click(2022,3)'>▶</a></h2>";
-calendar.innerHTML = text;
-*/
 
 class YC_Event{
     onDoubleClicked( date, isChecked ){}
@@ -28,14 +23,15 @@ class YCalendar{
         let prevDate = new Date( date.getFullYear(), date.getMonth(), date.getDate() );
         prevDate.setMonth( date.getMonth() - 1 );
 
-        let mainDate = new Date( date.getFullYear(), date.getMonth(), date.getDate() );
+        let mainDate_first = new Date( date.getFullYear(), date.getMonth(), 1 );
+        let mainDate_last  = new Date( date.getFullYear(), date.getMonth(), 0 );
 
         let nextDate = new Date( date.getFullYear(), date.getMonth(), date.getDate() );
         nextDate.setMonth( date.getMonth() + 1 );
 
         var text = "";
         text += '<h2 id="yc_title"><a title="' + prevDate.getFullYear() + '/' + (prevDate.getMonth() + 1) + '" onclick="prevButton_Click(' + prevDate.getFullYear() + ', ' + prevDate.getMonth() + ')">◀</a> ';
-        text += mainDate.getFullYear() + '/' + (mainDate.getMonth() + 1);
+        text += mainDate_first.getFullYear() + '/' + (mainDate_first.getMonth() + 1);
         text += ' <a title="' + nextDate.getFullYear() + '/' + (nextDate.getMonth() + 1) + '" onclick="nextButton_Click(' + nextDate.getFullYear() + ', ' + nextDate.getMonth() + ')">▶</a></h2>';
 
         calendar.innerHTML = text;
@@ -51,12 +47,10 @@ class YCalendar{
 const ycalendar = new YCalendar();
 
 function prevButton_Click( year, month ){
-    alert( "PrevButton_Click" + year + "年" + month + "月" );
     ycalendar.draw( new Date( year, month, 1 ) );
 }
 
 function nextButton_Click( year, month ){
-    alert( "NextButton_Click" + year + "年" + month + "月" );
     ycalendar.draw( new Date( year, month, 1 ) );
 }
 
