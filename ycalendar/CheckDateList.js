0d577934c8c5325c409d62548a2d744f249c4ef8
Yor-Jihons <ryo.malon814@gmail.com>
2022-11-10 19:44:37 +0900

    Separated to some js files.

---
+++ ycalendar/CheckDateList.js
@@ -0,0 +1,43 @@
+"use strict";
+
+class CheckDateList{
+    #dateExs;
+
+    constructor(){
+        this.#dateExs = new Array();
+    }
+
+    add( d ){
+        this.#dateExs.push( d );
+    }
+
+    clear(){
+        this.#dateExs.splice( 0 );
+    }
+
+    at( target ){
+        for( let i = 0; i < this.#dateExs.length; i++ ){
+            if( this.#dateExs[i].equals( target ) == true ){
+                return this.#dateExs[i];
+            }
+        }
+    return null;
+    }
+
+    /**
+     * 
+     * @param {*} target the object of the class DateEx, which you want to search.
+     * @returns Returns true if this object has the target, otherwise return false.
+     */
+    has( target ){
+        return(this.at( target ) == null ? false : true);
+    }
+
+    print(){
+        var txt = "";
+        for( let i = 0; i < this.#dateExs.length; i++ ){
+            txt += this.#dateExs[i].toString() + "\n";
+        }
+        alert( txt );
+    }
+}
---
+++ ycalendar/YC_Event.js
@@ -0,0 +1,5 @@
+"use strict";
+
+class YC_Event{
+    onDoubleClicked( year, month, day ){}
+}
