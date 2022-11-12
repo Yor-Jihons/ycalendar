9e2e65fb36a0ea4d5d50efd9b8004e38b5e37fff
Yor-Jihons <ryo.malon814@gmail.com>
2022-11-12 10:14:00 +0900

    Implemented the namespace in util.js

--- ycalendar/util.js
+++ ycalendar/util.js
@@ -1,13 +1,17 @@
 "use strict";
 
-/**
- * To create the class name string for the cells.
- * @param {*} checkedDateList The DateEx list you want to check.
- * @param {*} targetDate The object of the class DateEx you want to check.
- * @returns "yc_unchecked_day" or "yc_checked_day"
- */
-function createClassName( checkedDateList, targetDate ){
-    if( checkedDateList == undefined ) return "yc_unchecked_day";
-    if( checkedDateList.has( targetDate ) == true ) return "yc_checked_day";
-return "yc_unchecked_day";
-}
+(function(){
+    window.NameSpace1 = window.NameSpace1 || {};
+
+    /**
+    * To create the class name string for the cells.
+    * @param {*} checkedDateList The DateEx list you want to check.
+    * @param {*} targetDate The object of the class DateEx you want to check.
+    * @returns "yc_unchecked_day" or "yc_checked_day"
+    */
+    window.NameSpace1.createClassName = function( checkedDateList, targetDate ){
+        if( checkedDateList == undefined ) return "yc_unchecked_day";
+        if( checkedDateList.has( targetDate ) == true ) return "yc_checked_day";
+    return "yc_unchecked_day";
+    }
+})();
