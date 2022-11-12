04c8731f4679fa8c1c2bb0c4f7d65decdff531b3
Yor-Jihons <ryo.malon814@gmail.com>
2022-11-12 10:31:42 +0900

    Implemented the namespace in CheckDateList.js

--- ycalendar/CheckDateEx.js
+++ ycalendar/CheckDateEx.js
@@ -1,21 +1,25 @@
 "use strict";
 
-class CheckDateEx{
+(function(){
+    window.NameSpace1 = window.NameSpace1 || {};
 
-    #date;
+    window.NameSpace1.CheckDateEx = class{
+        #date;
 
-    constructor( year, month, day ){
-        this.#date = new Date( year, month, day, 1, 1, 1, 1 );
-    }
+        constructor( year, month, day ){
+            this.#date = new Date( year, month, day, 1, 1, 1, 1 );
+        }
 
-    equals( d ){
-        if( this.#date.getFullYear() == d.#date.getFullYear()
-        && this.#date.getMonth() == d.#date.getMonth()
-        && this.#date.getDate() == d.#date.getDate() ) return true;
-    return false;
-    }
+        equals( d ){
+            if( this.#date.getFullYear() == d.#date.getFullYear()
+            && this.#date.getMonth() == d.#date.getMonth()
+            && this.#date.getDate() == d.#date.getDate() ) return true;
+        return false;
+        }
 
-    toString(){
-        return this.#date.toDateString();
-    }
-}
+        toString(){
+            return this.#date.toDateString();
+        }
+    };
+})();
+
